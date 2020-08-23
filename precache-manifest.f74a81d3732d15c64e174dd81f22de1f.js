877c118c57ddefb52ff6c522a4ff437a54b7aa00
gregpmillr <gregpmillr@gmail.com>
2020-08-23 19:04:16 -0300

    Updates

---
+++ precache-manifest.f74a81d3732d15c64e174dd81f22de1f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "482a83e6f19e7fd7447b1bb2a195c76b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "09298919bcfe0e46d435",
+    "url": "/portfolio/static/css/main.f808ffd9.chunk.css"
+  },
+  {
+    "revision": "dd4f1cd026c9b3b8d71b",
+    "url": "/portfolio/static/js/2.7378b5c5.chunk.js"
+  },
+  {
+    "revision": "c50c25a3e91b902a9d4e9aafe6d9baf9",
+    "url": "/portfolio/static/js/2.7378b5c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09298919bcfe0e46d435",
+    "url": "/portfolio/static/js/main.497e669e.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "816c1e84f8002ed6a0eb6d47507f466e",
+    "url": "/portfolio/static/media/background.816c1e84.jpg"
+  },
+  {
+    "revision": "41d35a7b85d8dfad96e05ffe61c98329",
+    "url": "/portfolio/static/media/profileCircle.41d35a7b.png"
+  },
+  {
+    "revision": "085d08f60b4b8af48b1efaba1838013c",
+    "url": "/portfolio/static/media/yosemite.085d08f6.jpg"
+  }
+]);
